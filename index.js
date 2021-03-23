397a789394a054107f4d04649fdcc91c8742db0e
hoang.phanhuy <hoang.phanhuy@vietis.com.vn>
2021-03-23 10:07:32 +0700

    Revert "[New] New Proxy scheme"
    This reverts commit 3f43922359d16d91e373057be7fd6ee6619ded93.

--- index.js
+++ index.js
@@ -2,40 +2,28 @@ const express = require('express')
 const cors = require('cors')
 const fetch = require('node-fetch')
 
-const {
-  PORT = 8080,
-  DEFAULT_PORT = 80,
-  DEFAULT_HOST = '143.198.215.23'
-} = process.env
+const { PORT = 2301 } = process.env
 
 const app = express()
 
 app.use(cors())
 
-app.all('*', async (req, res) => {
-  const { method, headers, body, url } = req
-  const { port = DEFAULT_PORT, host = DEFAULT_HOST } = req.query
+app.get('/', async function (req, res) {
+  const { url } = req.query
 
-  try {
-    console.info(`${method}: http://${host}:${port}${url}`)
-    const fetchRes = await fetch(`http://${host}:${port}${url}`, {
-      method,
-      headers,
-      body
-    })
+  if (url) {
+    const { body, ok, status, text } = await fetch(url)
 
-    if (fetchRes.ok) {
-      fetchRes.body.pipe(res)
+    if (ok) {
+      body.pipe(res);
     } else {
-      res.status(fetchRes.status).send(await fetchRes.text())
+      res.status(status).send(await text())
     }
-  } catch (error) {
-    console.info(`fetch failed: http://${host}:${port}`)
-    console.error(error)
-    return res.status(500).send(error.message)
+  } else {
+    res.sendStatus(200)
   }
 })
 
 app.listen(PORT, function () {
-  console.log(`CORS-enabled web server listening on http://localhost:${PORT}`)
+  console.log(`CORS-enabled web server listening on port ${PORT}`)
 })
