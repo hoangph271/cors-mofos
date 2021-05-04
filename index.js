22c0fa384959579c614db3896f227b1d2be60be5
Huy Hoàng Phan <39024711+hoangph271@users.noreply.github.com>
2021-05-04 07:13:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,8 @@ const {
 
 const app = express()
 
-app.use(cors())
+app.use(cors('*'))
 app.use((req, res, next) => {
-//   if (!Number.parseInt(req.params.port, 10)) {
-//     return res.status(400).send('Invalid PORT number')
-//   }
-
   createProxyMiddleware({
     target: `http://${SERVER_IP}:${SERVER_PORT}`,
     changeOrigin: true
